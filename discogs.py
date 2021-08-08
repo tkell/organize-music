d9edd52d210de8c875d9bda5d3773506d5d72a0f
Thor Kell <thor@tide-pool.ca>
2021-08-08 14:40:58 -0400

    working release call, decent markdown

--- discogs.py
+++ discogs.py
@@ -54,6 +54,16 @@ if __name__ == "__main__":
 
             release_strings = []
             for release in releases:
+                # call discogs for release
+                release_url = release["basic_information"]["resource_url"]
+                release_data = call_discogs(release_url)
                 release_strings.append(make_release_string(release))
+                tracks = release_data["tracklist"]
+                for track in tracks:
+                    track_string = f"  {track['position']} - {track['title']} - {track['duration']}"
+                    release_strings.append(track_string)  # _hmmmm_
+                break  ## debug
+            print(release_strings)
             with open(output_file, "a") as f:
                 f.write(make_markdown_block(folder_name, release_strings))
+            break  ## debug
