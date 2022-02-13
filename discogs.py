60e447b824a371b2063fb5b3d6b31513f2d6b32e
Thor Kell <thor@tide-pool.ca>
2022-02-13 15:05:22 -0500

    add image and id, refactor parts, add retry

--- discogs.py
+++ discogs.py
@@ -26,15 +26,29 @@ def get_cache_expiry():
     return 60 * 60 * 24 * days
 
 
-def call_discogs_no_cache(url):
+def call_discogs_no_cache(url, is_retry=False):
     headers = {
         "user-agent": "DiscogsOrganize +http://tide-pool.ca",
         "Authorization": f"Discogs token={discogs_token}",
     }
     r = requests.get(url, headers=headers)
+
     time.sleep(2)
     print("calling: ", url)
-    return r.json()
+    try:
+        result = r.json()
+    except json.JSONDecodeError:
+        print("calling url failed:", url)
+        print("response reason", r.reason, r.status_code)
+        if r.status_code == 502 and is_retry == False:
+            time_to_sleep = random.randint(3, 6)
+            time.sleep(time_to_sleep)
+            result = call_discogs_no_cache(url, is_retry=True)
+        else:
+            print("calling url failed with Retry, panic!", url)
+            print("response reason", r.reason, r.status_code)
+
+    return result
 
 
 def call_discogs(url):
@@ -47,7 +61,27 @@ def call_discogs(url):
             return cached_data
     json_data = call_discogs_no_cache(url)
     discogs_cache[url] = (json_data, now)
+    # load / overwrite cache each time!
+    with open(cache_filename, "wb") as f:
+        pickle.dump(discogs_cache, f)
     return discogs_cache[url][0]
+
+
+def get_release_data(releases):
+    data = []
+    for release in releases:
+        release_url = release["basic_information"]["resource_url"]
+        release_data = call_discogs(release_url)
+        data.append((release, release_data))
+    return data
+
+
+def get_folder_name_and_releases(folder):
+    url = folder["resource_url"] + "/releases?per_page=100"
+    name = folder["name"].replace('""', "")
+    folder_data = call_discogs_no_cache(url)
+    releases = folder_data["releases"]
+    return name, releases
 
 
 def make_release_string(release):
@@ -62,11 +96,13 @@ def make_release_string(release):
 
 def make_release_json(release, folder_name):
     return {
+        "id": release["id"],
         "title": release["basic_information"]["title"],
         "artist": release["basic_information"]["artists"][0]["name"],
         "label": release["basic_information"]["labels"][0]["name"],
         "catno": release["basic_information"]["labels"][0]["catno"],
         "year": release["basic_information"]["year"],
+        "cover_image": release["basic_information"]["cover_image"],
         "folder": folder_name,
     }
 
@@ -89,19 +125,9 @@ def make_track_json(track):
     }
 
 
-def get_folder_name_and_releases(folder):
-    url = folder["resource_url"] + "/releases?per_page=100"
-    name = folder["name"].replace('""', "")
-    folder_data = call_discogs_no_cache(url)
-    releases = folder_data["releases"]
-    return name, releases
-
-
-def create_markdown_for_release(release):
+def create_markdown_for_release(release, release_data):
     release_strings = []
     release_strings.append(make_release_string(release))
-    release_url = release["basic_information"]["resource_url"]
-    release_data = call_discogs(release_url)
     tracks = release_data["tracklist"]
     for track in tracks:
         release_strings.append(make_track_string(track))
@@ -113,6 +139,7 @@ if __name__ == "__main__":
     output_json = "vinyl.json"
     try:
         os.remove(output_file)
+        os.remove(output_json)
     except FileNotFoundError:
         pass
 
@@ -126,12 +153,14 @@ if __name__ == "__main__":
     for folder in collection["folders"]:
         if folder["name"] not in ["All", "Uncategorized"]:
             folder_name, releases = get_folder_name_and_releases(folder)
-            markdown_strings = [create_markdown_for_release(r) for r in releases]
-            all_markdown_strings.append(markdown_block(folder_name, markdown_strings))
+            releases_and_release_data = get_release_data(releases)
 
-            for release in releases:
-                release_url = release["basic_information"]["resource_url"]
-                release_data = call_discogs(release_url)
+            folder_markdown_strings = []
+            for release, release_data in releases_and_release_data:
+                markdown_string = create_markdown_for_release(release, release_data)
+                folder_markdown_strings.append(markdown_string)
+
+            for release, release_data in releases_and_release_data:
                 json_entry = make_release_json(release, folder_name)
                 track_json = [
                     make_track_json(track) for track in release_data["tracklist"]
@@ -139,9 +168,11 @@ if __name__ == "__main__":
                 json_entry["tracks"] = track_json
                 all_json_data.append(json_entry)
 
+            all_markdown_strings.append(
+                markdown_block(folder_name, folder_markdown_strings)
+            )
+
     with open(output_file, "w") as f:
         f.write("\n".join(all_markdown_strings))
     with open(output_json, "w") as f:
         json.dump(all_json_data, f)
-    with open(cache_filename, "wb") as f:
-        pickle.dump(discogs_cache, f)
