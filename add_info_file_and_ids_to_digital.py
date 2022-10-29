8acaeb99b5550d52fc3b835954bd2e10b7f7eb40
Thor Kell <thor@tide-pool.ca>
2022-10-29 15:02:27 -0400

    incremental ids, harr harr

--- add_info_file_and_ids_to_digital.py
+++ add_info_file_and_ids_to_digital.py
@@ -7,17 +7,15 @@ if __name__ == "__main__":
     folders = os.listdir(albums_dir)
     print("listed directories")
 
+    # a bonus offset of 3 for windage
+    current_id = len(folders) + 3
     for folder in folders:
         if folder == ".DS_Store":
             continue
 
         folder_path = os.path.join(albums_dir, folder)
-        print(folder_path)
-
-        m = hashlib.sha256()
-        m.update(folder.encode("utf-8"))
-        id_integer = int(m.hexdigest()[0:24], 16)
-        info_dict = {"id": id_integer}
+        info_dict = {"id": current_id}
+        current_id += 1
 
         info_filepath = os.path.join(folder_path, "info.json")
         with open(info_filepath, "w") as f:
