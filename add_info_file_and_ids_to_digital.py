8ebe60acf6925d7319be2eaaebad6a688bafd55d
Thor Kell <thor@tide-pool.ca>
2022-10-29 15:38:32 -0400

    finally, proper ids

--- add_info_file_and_ids_to_digital.py
+++ add_info_file_and_ids_to_digital.py
@@ -10,14 +10,13 @@ if __name__ == "__main__":
     for folder in folders:
         if folder == ".DS_Store":
             continue
-
         folder_path = os.path.join(albums_dir, folder)
-        print(folder_path)
 
         m = hashlib.sha256()
         m.update(folder.encode("utf-8"))
-        id_integer = int(m.hexdigest()[0:24], 16)
+        id_integer = int(m.hexdigest()[0:8], 16)
         info_dict = {"id": id_integer}
+        print(folder_path, id_integer)
 
         info_filepath = os.path.join(folder_path, "info.json")
         with open(info_filepath, "w") as f:
