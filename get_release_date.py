ac1ac274dbd5f70b1fd646ddd47c829cc2783ad3
Thor Kell <thor@tide-pool.ca>
2024-05-12 11:32:42 -0400

    wip on release year, oh dear

--- get_release_date.py
+++ get_release_date.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+import re
 
 from src.discogs.lib_discogs import call_discogs
 from src.organize_music.local_file_io import read_info_file, write_info_file
@@ -35,14 +36,22 @@ if __name__ == "__main__":
         discogs_api_uri = f"https://api.discogs.com/releases/{discogs_id}"
         release_data = call_discogs(discogs_api_uri)
         year = release_data.get("year", None)
+        if not year:
+            year_string = release_data.get("released", "")
+            res = re.search(r"\d\d\d\d", year_string)
+            if res:
+                year = res.group(0)
+
         if year:
             print(f"Would be setting release year for {folder_path} to {year}")
             added += 1
+            i = input("Press y to write, n to skip")
+            if i == "y":
+                metadata = read_info_file(folder_path)
+                metadata["release_year"] = year
+                write_info_file(folder_path, metadata)
+        else:
+            print(f"Could not find release year for {folder_path} from Discogs")
+            missing += 1
 
-            continue  ## toooo scared to run this still!
-
-            metadata = read_info_file(folder_path)
-            metadata["release_year"] = year
-            write_info_file(folder_path, metadata)
-
-        print(missing, added)
+    print(missing, added)
