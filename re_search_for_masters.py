dc0a2f4c83c300efbe1e2fcb442eca139ed53ed4
Thor Kell <thor@tide-pool.ca>
2024-05-14 08:15:35 -0400

    tragic exception catch

--- re_search_for_masters.py
+++ re_search_for_masters.py
@@ -9,22 +9,26 @@ with open("strays.json", "r") as f:
 
 
 for folder_path in strays["master"]:
-    metadata = read_info_file(folder_path)
-    if "master" not in metadata["discogs_url"]:
-        continue
+    try:
+        metadata = read_info_file(folder_path)
+        if "master" not in metadata["discogs_url"]:
+            continue
 
-    release = folder_path.split("/")[-1]
-    artist = release.split(" - ")[0]
-    if artist == "Various Artists":
-        artist = "Various"
-    album_and_label = release.split(" - ")[1]
-    album = album_and_label.split(" [")[0]
-    if ":" in album:
-        album = album.replace(":", "/")
-    label = album_and_label.split(" [")[1].split("]")[0]
+        release = folder_path.split("/")[-1]
+        artist = release.split(" - ")[0]
+        if artist == "Various Artists":
+            artist = "Various"
+        album_and_label = release.split(" - ")[1]
+        album = album_and_label.split(" [")[0]
+        if ":" in album:
+            album = album.split(":")[0]
+        label = album_and_label.split(" [")[1].split("]")[0]
 
-    new_uri = search(artist, album, label)
+        new_uri = search(artist, album, label)
 
-    print(folder_path, new_uri)
-    metadata["discogs_url"] = new_uri
-    write_info_file(folder_path, metadata)
+        print(folder_path, new_uri)
+        metadata["discogs_url"] = new_uri
+        write_info_file(folder_path, metadata)
+    except Exception as e:
+        print("an error:", folder_path, e)
+        break
